3206309016affbe37f34a2ff4e838c64a7b9697b
Steven Nunez <snunez@cyrusinnovation.com>
2013-11-04 19:11:02 -0500

    Finishing Homework in class

--- 04_arrays/bob.rb
+++ 04_arrays/bob.rb
@@ -1,6 +1,27 @@
 class Bob
   def hey(message)
-    "Whatever."
+    @message = message
+
+    if screaming?
+      'Woah, chill out!'
+    elsif giving_silent_treatment?
+      'Fine. Be that way!'
+    elsif questioning?
+      'Sure.'
+    else
+      "Whatever."
+    end
   end
 
+  def questioning?
+    @message.end_with? '?'
+  end
+
+  def screaming?
+    @message == @message.upcase && @message =~ /[a-zA-Z]/
+  end
+
+  def giving_silent_treatment?
+    @message.strip.empty?
+  end
 end
